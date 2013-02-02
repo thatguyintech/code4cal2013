dd9766cd0db3fa54ea493a6f8a1aa4c7dd0f1a6f
Andrew Szeto <andrew@jabagawee.com>
2013-02-01 21:41:19 -0800

    added bs4 to figure the page out

--- downloader.py
+++ downloader.py
@@ -1,6 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
+from bs4 import BeautifulSoup
 import requests
 
 r = requests.get("http://students.berkeley.edu/osl/studentgroups/public/index.asp?todo=listgroups")
-page = r.text
+soup = BeautifulSoup(r.text)
+print(soup.prettify())
